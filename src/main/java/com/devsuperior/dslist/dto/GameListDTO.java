7707d5c67b43000ebcfdd7e56ff6170a5b2cd79b
Nathan Moreira <nathangc7@gmail.com>
2024-11-12 11:21:21 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public record GameListDTO(Long id, String name) {
+
+    public GameListDTO(GameList entity) {
+        this(entity.getId(), entity.getName());
+    }
+}
