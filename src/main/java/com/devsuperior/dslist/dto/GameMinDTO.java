69d9c3368d26d1d8f53eb26697fe9e428484107d
Nathan Moreira <nathangc7@gmail.com>
2024-11-12 11:54:50 -0300

    GET games by list

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,10 +1,15 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public record GameMinDTO(Long id, String title, Integer year, String imgUrl, String shortDescription) {
 
     public GameMinDTO(Game entity) {
         this(entity.getId(), entity.getTitle(), entity.getYear(), entity.getImgUrl(), entity.getShortDescription());
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        this(projection.getId(), projection.getTitle(), projection.getYear(), projection.getImgUrl(), projection.getShortDescription());
     }
 }
