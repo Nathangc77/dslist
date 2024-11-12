20656f5dd1573f3713e7c605cfd9e72b3468f927
Nathan Moreira <nathangc7@gmail.com>
2024-11-12 10:52:39 -0300

    GET game by id

---
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public record GameDTO(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
+
+    public GameDTO(Game entity) {
+         this(entity.getId(), entity.getTitle(), entity.getYear(), entity.getGenre(), entity.getPlatforms(), entity.getScore(), entity.getImgUrl(), entity.getShortDescription(), entity.getLongDescription());
+     }
+}
